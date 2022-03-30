992bf9a6e6d1332516c3cbf94b2067aaee9f47bd
Veronica M. Melendez Montiel <minetaky@gmail.com>
2022-03-29 19:25:02 -0600

    Se crea aplicacion para formularios template y reactivos, se crean las rutas y lazyload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReactiveModule } from './reactive/reactive.module';
+import { SharedModule } from './shared/shared.module';
+import { TemplateModule } from './template/template.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'reactive',
+    loadChildren: () => import('./reactive/reactive.module').then( m => ReactiveModule)
+  },
+  {
+    path: 'template',
+    loadChildren: () => import('./template/template.module').then( m => TemplateModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'template'
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
