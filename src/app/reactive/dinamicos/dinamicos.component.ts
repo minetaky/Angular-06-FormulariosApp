2967e7bcb79d966afd018f4679604b8c894f7aad
Veronica M. Melendez Montiel <minetaky@gmail.com>
2022-03-31 17:58:22 -0600

    Se borran los favoritos

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -10,8 +10,18 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class DinamicosComponent {
 
   miFormulario: FormGroup = this.fb.group({
-    nombre: [ '', [ Validators.required, Validators.minLength(3) ] ]
-  })
+    nombre: [ '', [ Validators.required, Validators.minLength(3) ] ],
+    favoritos: this.fb.array([
+      [ 'Con olor a fresas' ],
+      [ 'Como aman los hombres' ]
+    ], Validators.required)
+  });
+
+  nuevoFavorito: FormControl = this.fb.control('', Validators.required)
+
+  get getFavoritosArr(){
+    return this.miFormulario.get('favoritos') as FormArray;
+  }
 
   constructor(private fb: FormBuilder) { }
 
@@ -32,6 +42,25 @@ export class DinamicosComponent {
     return this.miFormulario.controls[campo].touched &&
            this.miFormulario.controls[campo].errors;
 
+  }
+
+  agregarFavorito(){
+    if( this.nuevoFavorito.invalid ){ return; }
+
+    //this.getFavoritosArr.push( new FormControl( this.nuevoFavorito.value, Validators.required ) );
+    this.getFavoritosArr.push( this.fb.control( this.nuevoFavorito.value, Validators.required ) );
+
+    this.nuevoFavorito.reset();
+  }
+
+
+  borrar(i: number){
+    //if( this.nuevoFavorito.invalid ){ return; }
+
+    //this.getFavoritosArr.push( new FormControl( this.nuevoFavorito.value, Validators.required ) );
+    this.getFavoritosArr.removeAt(i);
+
+    this.nuevoFavorito.reset();
   }
 
 
