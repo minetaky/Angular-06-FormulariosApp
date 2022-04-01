be958a76a85b5d2f1d3ed0434a4a8234060a6814
Veronica M. Melendez Montiel <minetaky@gmail.com>
2022-04-01 11:40:50 -0600

    Switch con Reactivos

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -8,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup = this._fb.group({
+
+    genero: [ 'M', Validators.required ],
+    notificaciones: [ true, Validators.required ],
+    condiciones: [false, Validators.requiredTrue ]
+  });
+
+  persona = {
+    genero: 'F',
+    notificaciones: true
+  }
+
+  constructor(private _fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      ...this.persona,
+      condiciones: true
+    } );
+
+    this.miFormulario.valueChanges.subscribe( form => { 
+      delete form.condiciones;
+      this.persona = form;
+     } )
+
+  }
+
+  guardar(){
+    const formValue = { ...this.miFormulario.value };
+    delete formValue.condiciones;
+
+    this.persona = formValue;
+
   }
 
 }
